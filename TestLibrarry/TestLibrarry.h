06f33afdca31542492b38be64e6df811a97a0c05
HirenBharodiya <hiren.bharodia@gmail.com>
2024-08-29 16:16:44 +0530

    Initial project setup

---
+++ TestLibrarry/TestLibrarry.h
@@ -0,0 +1,18 @@
+//
+//  TestLibrarry.h
+//  TestLibrarry
+//
+//  Created by user on 29/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestLibrarry.
+FOUNDATION_EXPORT double TestLibrarryVersionNumber;
+
+//! Project version string for TestLibrarry.
+FOUNDATION_EXPORT const unsigned char TestLibrarryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestLibrarry/PublicHeader.h>
+
+
---
+++ TestLibraryExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestLibraryExample
+//
+//  Created by user on 29/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
